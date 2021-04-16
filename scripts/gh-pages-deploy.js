80022deeb8b8f1fd97629dbeef83882ee692c819
Oier Lauzirika Zarrabeitia <oierlauzi@riseup.net>
2021-04-16 20:19:07 +0200

    404 page workaround

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -9,21 +9,24 @@ const fs = require("fs");
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    // eslint-disable-next-line no-console
+
+    //Build the project
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
-    // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    const buildFolder = fs.existsSync("dist") ? "dist/" : "build/";
+    fs.copyFileSync(buildFolder + "index.html", buildFolder + "404.html");
+
+    //Deploy on GH pages    
+    await execa("git", ["--work-tree", buildFolder, "add", "--all"]);
+    await execa("git", ["--work-tree", buildFolder, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rm", ["-r", folderName]);
+    await execa("rm", ["-r", buildFolder]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
+
   } catch (e) {
-    // eslint-disable-next-line no-console
     console.log(e.message);
     process.exit(1);
   }
