3a9965134279a80032d52aa2fbbb4de0088eb046
Oier Lauzirika Zarrabeitia <oierlauzi@riseup.net>
2021-04-17 16:08:56 +0200

    Added WebSocket

--- src/components/Connection.vue
+++ src/components/Connection.vue
@@ -1,26 +1,27 @@
 <template>
   <div>
-    <b-form inline>
+    <!-- Form -->
+    <b-form inline ref="form">
       <label class="sr-only" for="input-url">Cenital URL</label>
       <b-input-group 
         prepend="URL" 
-        class="mb-2 mr-sm-2 mb-sm-0"
-        :state="state"
       >
-        <b-form-input 
-          id="input-url" 
-          placeholder="Cenital URL"
+        <b-form-input
+          ref="input-url" 
+          placeholder="ws://example.io/"
+          type="url"
           v-model="url"
-          :state="state"
         />
       </b-input-group>
 
       <b-button 
-        variant="primary"
-        @click="handleConnect"
+        ref="button-connect" 
+        :variant="connectionState ? 'success' : 'danger'"
+        @click="connect"
       >
         Connect
       </b-button>
+
     </b-form>
 
   </div>
@@ -30,17 +31,67 @@
   export default {
     name: "Connection",
     components: {},
-    props: {
-      state: { type: Boolean },
-      url: { type: String }
-    },
+    props: {},
     data() {
-      return {}
+      return {
+        url: "",
+        socket: null
+      }
     },
     methods: {
-      handleConnect() {
-        //Emit the connect event
-        this.$emit("connect");
+      send(data) {
+        if(this.socket) {
+          this.socket.send(data);
+        }
+      },
+      connect() {
+        //Create a callback for opening the new 
+        //conection when the old one is closed
+        var self = this;
+        var connectCallback = function() {
+          self.socket = null;
+
+          //Check if the url is valid
+          if(self.url && self.$refs['input-url'].checkValidity()) {
+            //Create the new connection
+            self.socket = new WebSocket(self.url)
+
+            //Configure the events
+            self.socket.onopen = function() {
+              //Emit the connection event
+              self.$emit('connected')
+            }
+            self.socket.onmessage = function(event) {
+              //Emit the event data
+              self.$emit('recv', event.data)
+            }
+            self.socket.onclose = function() {
+              //Set it as invalid
+              self.socket = null
+
+              //Show the error closed modal
+              self.$bvModal.msgBoxOk("Connection closed with " + this.url)
+
+              //Emit the disconnection event
+              self.$emit('disconnected')
+            }
+          }
+        }
+
+        //Destroy the old connection
+        if(this.socket) {
+          this.socket.onclose = connectCallback;
+          this.socket.close();
+        } else {
+          //Directly call the connect callback
+          connectCallback()
+        }
+
+      }
+    },
+    computed: {
+      connectionState() {
+        return this.socket !== null
       }
     }
   }
