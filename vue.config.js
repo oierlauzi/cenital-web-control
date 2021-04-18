ceb882a5365f05adf4de7de7324dddf952a3b76e
Oier Lauzirika Zarrabeitia <oierlauzi@riseup.net>
2021-04-18 15:57:11 +0200

    Modified the application <title>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-	publicPath: '/cenital-web-control/'
+  publicPath: '/cenital-web-control/',
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = "Cenital Web Control"
+      return args;
+    })
+  }
 }
