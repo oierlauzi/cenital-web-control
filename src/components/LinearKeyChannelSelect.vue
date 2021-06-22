45905a9b8b3ed2c5edcc959e6d3a97d27c57f6f4
Oier Lauzirika Zarrabeitia <oierlauzi@riseup.net>
2021-06-22 14:48:41 +0200

    Added keying controls

---
+++ src/components/LinearKeyChannelSelect.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <b-form-select 
+      :value="value" 
+      :options="options" 
+      @input="onInput" 
+      @change="onChange"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "LinearKeyChannelSelect",
+    components: {},
+    props: {
+      value: { type: String, default: "fillR" },
+    },
+    data() {
+      return {};
+    },
+    methods: {
+      onInput(value) {
+        this.$emit("input", value);
+      },
+      onChange(value) {
+        this.$emit("change", value);
+      },
+    },
+    computed: {
+      options() {
+        return [
+          { value: "fillR", text: "Fill R" },
+          { value: "fillG", text: "Fill G" },
+          { value: "fillB", text: "Fill B" },
+          { value: "fillA", text: "Fill A" },
+          { value: "fillY", text: "Fill Y" },
+          { value: "keyR",  text: "Key R" },
+          { value: "keyG",  text: "Key G" },
+          { value: "keyB",  text: "Key B" },
+          { value: "keyA",  text: "Key A" },
+          { value: "keyY",  text: "Key Y" },
+        ];
+      }
+    }
+  };
+</script>
--- src/components/MixEffectOverlay.vue
+++ src/components/MixEffectOverlay.vue
@@ -1,3 +1,9 @@
+<style scoped>
+  .slider-rainbow {
+    background: linear-gradient(to right,hsl(0,100%,50%),hsl(60,100%,50%),hsl(120,100%,50%),hsl(180,100%,50%),hsl(240,100%,50%),hsl(300,100%,50%),hsl(360,100%,50%));
+  }
+</style>
+
 <template>
   <div>
     <b-container fluid>
@@ -169,13 +175,326 @@
         </b-col>
       </b-row>
 
+      <!-- Linear key title -->
+      <b-row class="my-3">
+        <center><h3> Linear key </h3></center>
+      </b-row>
+
+      <!-- Linear key enabled -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="check-linear-key-enabled">
+            Enabled:
+          </label>
+        </b-col>
+        <b-col sm="9">
+          <b-form-checkbox switch
+            ref="check-linear-key-enabled"
+            :checked="linearKeyEnabled"
+            @change="onLinearKeyEnabledChange"
+          />
+        </b-col>
+      </b-row>
+
+      <!-- Linear key inverted -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="check-linear-key-inverted">
+            Inverted:
+          </label>
+        </b-col>
+        <b-col sm="9">
+          <b-form-checkbox switch
+            ref="check-linear-key-inverted"
+            :checked="linearKeyInverted"
+            @change="onLinearKeyInvertedChange"
+          />
+        </b-col>
+      </b-row>
+
+      <!-- Linear key channel -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="select-linear-key-channel">
+            Channel:
+          </label>
+        </b-col>
+        <b-col sm="9">
+          <LinearKeyChannelSelect
+            ref="select-linear-key-channel"
+            :value="linearKeyChannel"
+            @change="onLinearKeyChannelChange"
+          />
+        </b-col>
+      </b-row>
+
+      <!-- Luma key title -->
+      <b-row class="my-3">
+        <center><h3> Luma key </h3></center>
+      </b-row>
+
+      <!-- Luma key enabled -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="check-luma-key-enabled">
+            Enabled:
+          </label>
+        </b-col>
+        <b-col sm="9">
+          <b-form-checkbox switch
+            ref="check-luma-key-enabled"
+            :checked="lumaKeyEnabled"
+            @change="onLumaKeyEnabledChange"
+          />
+        </b-col>
+      </b-row>
+
+      <!-- Luma key inverted -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="check-luma-key-inverted">
+            Inverted:
+          </label>
+        </b-col>
+        <b-col sm="9">
+          <b-form-checkbox switch
+            ref="check-luma-key-inverted"
+            :checked="lumaKeyInverted"
+            @change="onLumaKeyInvertedChange"
+          />
+        </b-col>
+      </b-row>
+
+      <!-- Luma key min threshold -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-luma-key-min">
+            Min:
+          </label>
+        </b-col>
+        <b-col sm="7">
+          <b-form-input
+            ref="input-luma-key-min"
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            :value="lumaKeyMinThreshold"
+            @input="onLumaKeyMinThresholdInput"
+          />
+        </b-col>
+        <b-col sm="2">
+          {{lumaKeyMinThreshold}}
+        </b-col>
+      </b-row>
+      
+      <!-- Luma key max threshold -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-luma-key-max">
+            Max:
+          </label>
+        </b-col>
+        <b-col sm="7">
+          <b-form-input
+            ref="input-luma-key-max"
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            :value="lumaKeyMaxThreshold"
+            @input="onLumaKeyMaxThresholdInput"
+          />
+        </b-col>
+        <b-col sm="2">
+          {{lumaKeyMaxThreshold}}
+        </b-col>
+      </b-row>
+
+      <!-- Chroma key title -->
+      <b-row class="my-3">
+        <center><h3> Chroma key </h3></center>
+      </b-row>
+
+      <!-- Chroma key enabled -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="check-chroma-key-enabled">
+            Enabled:
+          </label>
+        </b-col>
+        <b-col sm="9">
+          <b-form-checkbox switch
+            ref="check-chroma-key-enabled"
+            :checked="chromaKeyEnabled"
+            @change="onChromaKeyEnabledChange"
+          />
+        </b-col>
+      </b-row>
+
+      <!-- Chroma key hue center -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-chroma-key-hue-center">
+            Hue center:
+          </label>
+        </b-col>
+        <b-col sm="7">
+          <b-form-input
+            ref="input-chroma-key-hue-center"
+            type="range"
+            class="slider-rainbow"
+            min="0"
+            max="360"
+            step="1"
+            :value="chromaKeyHue"
+            @input="onChromaKeyHueInput"
+          />
+        </b-col>
+        <b-col sm="2">
+          {{chromaKeyHue}} &deg;
+        </b-col>
+      </b-row>
+
+      <!-- Chroma key hue span -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-chroma-key-hue-span">
+            Hue span:
+          </label>
+        </b-col>
+        <b-col sm="7">
+          <b-form-input
+            ref="input-chroma-key-hue-span"
+            type="range"
+            min="0"
+            max="180"
+            step="1"
+            :value="chromaKeyHueSpan"
+            @input="onChromaKeyHueSpanInput"
+          />
+        </b-col>
+        <b-col sm="2">
+          {{chromaKeyHueSpan}} &deg;
+        </b-col>
+      </b-row>
+
+      <!-- Chroma key hue smoothness -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-chroma-key-hue-smoothness">
+            Hue smoothness:
+          </label>
+        </b-col>
+        <b-col sm="7">
+          <b-form-input
+            ref="input-chroma-key-hue-smoothness"
+            type="range"
+            min="0"
+            max="180"
+            step="1"
+            :value="chromaKeyHueSmoothness"
+            @input="onChromaKeyHueSmoothnessInput"
+          />
+        </b-col>
+        <b-col sm="2">
+          {{chromaKeyHueSmoothness}} &deg;
+        </b-col>
+      </b-row>
+
+      <!-- Chroma key saturation threshold -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-chroma-key-sat-threshold">
+            Saturation threshold:
+          </label>
+        </b-col>
+        <b-col sm="7">
+          <b-form-input
+            ref="input-chroma-key-sat-threshold"
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            :value="chromaKeySaturationThreshold"
+            @input="onChromaKeySaturationThresholdInput"
+          />
+        </b-col>
+        <b-col sm="2">
+          {{chromaKeySaturationThreshold}}
+        </b-col>
+      </b-row>
+
+      <!-- Chroma key saturation smoothness -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-chroma-key-sat-smoothness">
+            Saturation smoothness:
+          </label>
+        </b-col>
+        <b-col sm="7">
+          <b-form-input
+            ref="input-chroma-key-sat-smoothness"
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            :value="chromaKeySaturationSmoothness"
+            @input="onChromaKeySaturationSmoothnessInput"
+          />
+        </b-col>
+        <b-col sm="2">
+          {{chromaKeySaturationSmoothness}}
+        </b-col>
+      </b-row>
+
+      <!-- Chroma key brightness threshold -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-chroma-key-val-threshold">
+            Brightness threshold:
+          </label>
+        </b-col>
+        <b-col sm="7">
+          <b-form-input
+            ref="input-chroma-key-val-threshold"
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            :value="chromaKeyBrightnessThreshold"
+            @input="onChromaKeyBrightnessThresholdInput"
+          />
+        </b-col>
+        <b-col sm="2">
+          {{chromaKeyBrightnessThreshold}}
+        </b-col>
+      </b-row>
+
+      <!-- Chroma key brightness smoothness -->
+      <b-row class="my-1">
+        <b-col sm="3">
+          <label for="input-chroma-key-val-smoothness">
+            Brightness smoothness:
+          </label>
+        </b-col>
+        <b-col sm="7">
+          <b-form-input
+            ref="input-chroma-key-val-smoothness"
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            :value="chromaKeyBrightnessSmoothness"
+            @input="onChromaKeyBrightnessSmoothnessInput"
+          />
+        </b-col>
+        <b-col sm="2">
+          {{chromaKeyBrightnessSmoothness}}
+        </b-col>
+      </b-row>
+
     </b-container>
-
-
-
-
-    <h3> TODO: {{ overlaySlot.toUpperCase() }} Keyer {{ index }} </h3>
-
   </div>
 </template>
 
@@ -189,6 +508,7 @@
   import BlendingModeSelect from './BlendingModeSelect'
   import ScalingModeSelect from './ScalingModeSelect'
   import ScalingFilterSelect from './ScalingFilterSelect'
+  import LinearKeyChannelSelect from './LinearKeyChannelSelect'
 
   export default {
     name: "MixEffectKeyer",
@@ -197,6 +517,7 @@
       BlendingModeSelect,
       ScalingModeSelect,
       ScalingFilterSelect,
+      LinearKeyChannelSelect
     },
     props: {
       name: { type: String, required: true },
@@ -250,9 +571,55 @@
       onScalingFilterChange(value) {
         this.$store.dispatch('mixEffect/setOverlayScalingFilter', { name: this.name, slot: this.overlaySlot, index: this.index, value });
       },
+      onLinearKeyEnabledChange(value) {
+        this.$store.dispatch('mixEffect/setLinearKeyEnabled', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onLinearKeyInvertedChange(value) {
+        this.$store.dispatch('mixEffect/setLinearKeyInverted', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onLinearKeyChannelChange(value) {
+        this.$store.dispatch('mixEffect/setLinearKeyChannel', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onLumaKeyEnabledChange(value) {
+        this.$store.dispatch('mixEffect/setLumaKeyEnabled', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onLumaKeyInvertedChange(value) {
+        this.$store.dispatch('mixEffect/setLumaKeyInverted', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onLumaKeyMinThresholdInput(value) {
+        this.$store.dispatch('mixEffect/setLumaKeyMinThreshold', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onLumaKeyMaxThresholdInput(value) {
+        this.$store.dispatch('mixEffect/setLumaKeyMaxThreshold', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onChromaKeyEnabledChange(value) {
+        this.$store.dispatch('mixEffect/setChromaKeyEnabled', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onChromaKeyHueInput(value) {
+        this.$store.dispatch('mixEffect/setChromaKeyHue', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onChromaKeyHueSpanInput(value) {
+        this.$store.dispatch('mixEffect/setChromaKeyHueSpan', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onChromaKeyHueSmoothnessInput(value) {
+        this.$store.dispatch('mixEffect/setChromaKeyHueSmoothness', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onChromaKeySaturationThresholdInput(value) {
+        this.$store.dispatch('mixEffect/setChromaKeySaturationThreshold', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onChromaKeySaturationSmoothnessInput(value) {
+        this.$store.dispatch('mixEffect/setChromaKeySaturationSmoothness', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onChromaKeyBrightnessThresholdInput(value) {
+        this.$store.dispatch('mixEffect/setChromaKeyBrightnessThreshold', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+      onChromaKeyBrightnessSmoothnessInput(value) {
+        this.$store.dispatch('mixEffect/setChromaKeyBrightnessSmoothness', { name: this.name, slot: this.overlaySlot, index: this.index, value });
+      },
+
     },
     computed: {
-      ...mapGetters("mixEffect", [ 
+      ...mapGetters('mixEffect', [ 
         'getOverlayVisible',
         'getOverlayTransition',
         'getOverlayPosition',
@@ -262,6 +629,21 @@
         'getOverlayBlendingMode',
         'getOverlayScalingMode',
         'getOverlayScalingFilter',
+        'getLinearKeyEnabled',
+        'getLinearKeyInverted',
+        'getLinearKeyChannel',
+        'getLumaKeyEnabled',
+        'getLumaKeyInverted',
+        'getLumaKeyMinThreshold',
+        'getLumaKeyMaxThreshold',
+        'getChromaKeyEnabled',
+        'getChromaKeyHue',
+        'getChromaKeyHueSpan',
+        'getChromaKeyHueSmoothness',
+        'getChromaKeySaturationThreshold',
+        'getChromaKeySaturationSmoothness',
+        'getChromaKeyBrightnessThreshold',
+        'getChromaKeyBrightnessSmoothness',
       ]),
 
       fillInSelection() {
@@ -316,6 +698,51 @@
       },
       scalingFilter() {
         return this.getOverlayScalingFilter(this.name, this.overlaySlot, this.index);
+      },
+      linearKeyEnabled() {
+        return this.getLinearKeyEnabled(this.name, this.overlaySlot, this.index);
+      },
+      linearKeyInverted() {
+        return this.getLinearKeyInverted(this.name, this.overlaySlot, this.index);
+      },
+      linearKeyChannel() {
+        return this.getLinearKeyChannel(this.name, this.overlaySlot, this.index);
+      },
+      lumaKeyEnabled() {
+        return this.getLumaKeyEnabled(this.name, this.overlaySlot, this.index);
+      },
+      lumaKeyInverted() {
+        return this.getLumaKeyInverted(this.name, this.overlaySlot, this.index);
+      },
+      lumaKeyMinThreshold() {
+        return this.getLumaKeyMinThreshold(this.name, this.overlaySlot, this.index);
+      },
+      lumaKeyMaxThreshold() {
+        return this.getLumaKeyMaxThreshold(this.name, this.overlaySlot, this.index);
+      },
+      chromaKeyEnabled() {
+        return this.getChromaKeyEnabled(this.name, this.overlaySlot, this.index);
+      },
+      chromaKeyHue() {
+        return this.getChromaKeyHue(this.name, this.overlaySlot, this.index);
+      },
+      chromaKeyHueSpan() {
+        return this.getChromaKeyHueSpan(this.name, this.overlaySlot, this.index);
+      },
+      chromaKeyHueSmoothness() {
+        return this.getChromaKeyHueSmoothness(this.name, this.overlaySlot, this.index);
+      },
+      chromaKeySaturationThreshold() {
+        return this.getChromaKeySaturationThreshold(this.name, this.overlaySlot, this.index);
+      },
+      chromaKeySaturationSmoothness() {
+        return this.getChromaKeySaturationSmoothness(this.name, this.overlaySlot, this.index);
+      },
+      chromaKeyBrightnessThreshold() {
+        return this.getChromaKeyBrightnessThreshold(this.name, this.overlaySlot, this.index);
+      },
+      chromaKeyBrightnessSmoothness() {
+        return this.getChromaKeyBrightnessSmoothness(this.name, this.overlaySlot, this.index);
       },
     }
   };
